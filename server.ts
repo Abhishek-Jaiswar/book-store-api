eebba30f60b1408fe2b88adfed1321a7ccfeef72
Abhishek Jaiswar <abhisheknduw@gmail.com>
2024-07-19 07:39:05 +0530

    add dotenv

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
-import app from "./app";
+import app from "./src/app";
+import dotenv from 'dotenv'
 
-const PORT = 3000
+dotenv.config()
+
+const Port = process.env.PORT || 3000
 
 const startServer = async () => {
     try {
-        app.listen(PORT, () => {
-            console.log(`Server is listening on http://localhost:${PORT}`);
+        app.listen(Port, () => {
+            console.log(`Server is listening on http://localhost:${Port}`);
         })
     } catch (error: any) {
         console.log("Failed to start server");
